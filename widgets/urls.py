4304e7f267ad7e645a6c02595b82f234ff28a47f
Leigh Forrest <leighmforrest@gmail.com>
2019-09-22 11:10:51 -0400

    delete view addded, more ui tweaks

--- widgets/urls.py
+++ widgets/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from widgets.views import WidgetListView, WidgetDetailView, CommentCreateView
+from widgets.views import (
+    WidgetListView,
+    WidgetDetailView,
+    CommentCreateView,
+    CommentUpdateView,
+)
 
 app_name = "widgets"
 
@@ -8,5 +13,6 @@ urlpatterns = [
     path("", WidgetListView.as_view(), name="index"),
     path("<uuid:pk>", WidgetDetailView.as_view(), name="detail"),
     path("<uuid:pk>/create", CommentCreateView.as_view(), name="comment_create"),
+    path("<uuid:pk>/update", CommentUpdateView.as_view(), name="comment_update"),
 ]
 
--- widgets/views.py
+++ widgets/views.py
@@ -1,10 +1,12 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
 
 
-from widgets.models import Widget
+from widgets.models import Widget, Comment
 from widgets.forms import CommentForm
 
 
@@ -39,3 +41,50 @@ class CommentCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
         form.instance.user = self.request.user
         form.instance.widget = Widget.objects.get(pk=self.kwargs["pk"])
         return super().form_valid(form)
+
+
+class CommentUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    template_name = "widgets/comment_update.html"
+    model = Comment
+    form_class = CommentForm
+    success_message = "The comment has been updated."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["widget"] = Comment.objects.get(pk=self.kwargs["pk"]).widget
+        return context
+
+    def get_success_url(self):
+        comment = self.get_object()
+        return comment.widget.get_absolute_url()
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.widget = Comment.objects.get(pk=self.kwargs["pk"]).widget
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = "widgets/comment_delete.html"
+
+    def get_success_url(self):
+        comment = self.get_object()
+        return comment.widget.get_absolute_url()
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        message = f"The comment has been deleted."
+        messages.warning(self.request, message)
+        return super().delete(request, *args, **kwargs)
